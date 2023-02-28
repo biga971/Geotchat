1387932ef550a73de564b954f8bc2805d4e7df09
sabassi <abassisteven@gmail.com>
2023-02-28 19:03:14 -0400

    2719h

--- www/js/home.js
+++ www/js/home.js
@@ -2,7 +2,13 @@
 document.addEventListener('deviceready', onDeviceReady, true);
 
 var sreen = "b1"
+var stevenboxconversation = document.getElementById("boxconvsteven")
+var talkto;
+var idtalkto;
+var e;
 
+//var pagemessage = window.location.href="message.html"
+var utilisateur=JSON.parse(sessionStorage.getItem('utilisateur2'))
 function onDeviceReady() {
 
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version)
@@ -13,6 +19,13 @@ function onDeviceReady() {
     document.getElementById('picture').addEventListener('click', takePic, false);
     document.getElementById('buttonLocation').addEventListener('click', takeLocation, false);
 }
+
+
+function printu(){
+   
+    console.log(utilisateur)
+
+} 
 
 
 function showTable() {
@@ -99,7 +112,7 @@ function onFail(message) {
 }
 
 
-function takeLocation () {
+/*function takeLocation () {
     
     const options = {
         maximumAge: 3000,
@@ -117,7 +130,7 @@ function takeLocation () {
             'Speed: '             + position.coords.speed             + '\n' +
             'Timestamp: '         + position.timestamp                + '\n');
     };
-
+fli
     // onError Callback receives a PositionError object
     //
     function onError(error) {
@@ -126,7 +139,7 @@ function takeLocation () {
     }
 
     navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
-}
+}*/
 
 
 function showPictureWebService (data) {
@@ -159,14 +172,67 @@ function display(value) {
 
 }
 
-function like() {
-    const siwpeBox = document.querySelector('.siwpeBox')
-    siwpeBox.style.transform = `translateX(500px) rotateY(40deg)`
+function effacerswipe(div){
+    div.remove()
+    console.log('dsdsdooooo')
+}
+function afficherswipe(){
+    if(document.querySelectorAll('.swipebox').length<=1){
+        lireswipe();
+    }
+    if(document.querySelector('.swipebox')!=null){
+        document.querySelector('.swipebox').style.opacity='1'
+        console.log(document.querySelector('.swipebox').id)
+    }
+
 }
 
-function dislike() {
-    const siwpeBox = document.querySelector('.siwpeBox')
-    siwpeBox.style.transform = `translateX(-500px) rotateY(-40deg)`
+function like(id) {
+    //const siwpeBox = document.querySelector('.siwpeBox')
+    //siwpeBox.style.transform = `translateX(500px) rotateY(40deg)`
+
+    var swipebox = document.getElementById('swipebox'+id)
+    
+    swipebox.style.transform = `translateX(500px) rotateY(40deg)`
+    setTimeout(function(){ effacerswipe(swipebox);
+        if(document.querySelectorAll('.swipebox').length<=1){
+        lireswipe();
+        }
+        if(document.querySelector('.swipebox')!=null){
+//            document.querySelector('.swipebox').style.opacity='1'
+            document.querySelector('.swipebox').style.display='flex'
+            
+            console.log(document.querySelector('.swipebox').id)
+        }},1000)
+    //swipebox.style.opacity = "0";
+
+    var url = "https://proj.ruben-jeaurat.fr/api/modifyinvi?idInviteur"+utilisateur["idGeotchatteur"]+"&idInvite="+id+"&dateInvitation="+Date.now()+"&idEtat=2"
+    fetch(url, {
+    method: 'get',
+    headers: { 'Accept': 'application/json',"Content-Type": "multipart/form-data" 
+
+    },
+    //body: formData
+    })
+    
+    .catch(response=>console.log(response))
+    
+    
+    
+   
+	
+
+   
+}
+
+function dislike(id) {
+    //const siwpeBox = document.querySelector('.siwpeBox')
+    //siwpeBox.style.transform = `translateX(-500px) rotateY(-40deg)`
+
+    var swipebox = document.getElementById('swipebox'+id)
+    swipebox.style.transform = `translateX(-500px) rotateY(-40deg)`
+    setTimeout(function(){ effacerswipe(swipebox); },1600)
+
 }
 
 function signUp() {
@@ -210,3 +276,304 @@ function signUp() {
      }
        
  }
+  
+ function lireconversation(){
+    //console.log(utilisateur)
+    const url = "https://proj.ruben-jeaurat.fr/selectinvi"
+    
+    var invit=[];
+    
+    
+    //window.location='./home.html'  
+    fetch(url, {
+        headers: { 'Accept': 'application/json'
+    },
+})
+    .then(response => response.json())
+    .then(response =>  {
+        stevenboxconversation.innerHTML='';
+        for (var i = 0; i < response.length; i++){
+
+
+            if(response[i]["idEtat"]==1){
+
+                if(response[i]["idInvite"] == utilisateur["idGeotchatteur"] || response[i]["idInviteur"] == utilisateur["idGeotchatteur"] ){
+                    invit.push(response[i])
+
+
+                }
+            }
+
+
+        }
+
+    })
+    .then(response =>{  for (var i = 0; i < invit.length; i++){
+        let row=document.createElement('div');
+                //utilisateur[0]["idGeotchatteur"]==invit[0]["idGeotchatteur"]? p_msg.className="from-me" : p_msg.className="from-them"
+        console.log(invit[i])
+                //invit[0]["idInviteur"]
+        talkto = utilisateur["idGeotchatteur"] ==invit[i]["idInvite"] ?  invit[i]["idInviteur"]:invit[i]["idInvite"] ;
+        row.setAttribute('onclick','sessionStorage.setItem("idtalkto",'+talkto+');console.log(sessionStorage.getItem("idtalkto"));window.location="./message.html"; /*liremsg('+talkto+')*/');
+        row.setAttribute('className','Row ' + talkto)
+                //row.innerHTML=talkto;
+
+        let rowimg=document.createElement('img')
+        rowimg.setAttribute('className','imagconv')
+
+        let Boxrow=document.createElement('div')
+        Boxrow.className="BoxRow"
+
+        let afterBoxrow=document.createElement('div')
+
+        let h1=document.createElement('h1')
+
+        let span=document.createElement('span')
+
+        let span2=document.createElement('span')
+
+        let hr=document.createElement('hr')
+
+        fetch("https://proj.ruben-jeaurat.fr/selectgeo?id="+talkto, 
+        {
+            headers: { 'Accept': 'application/json'
+        },
+    })
+        .then(response => response.json())
+    .then(response=>{console.log(response[0]);
+         rowimg.src=response[0]["photo"];
+        h1.innerHTML=response[0]["prenom"]})
+        row.appendChild(rowimg)
+        row.appendChild(Boxrow)
+        Boxrow.appendChild(afterBoxrow)
+        Boxrow.appendChild(span2)
+        afterBoxrow.appendChild(h1)
+        afterBoxrow.appendChild(span)
+        stevenboxconversation.appendChild(row);
+        stevenboxconversation.appendChild(hr)
+        
+    
+    
+        
+
+}
+
+
+})
+
+
+    
+}
+
+ function lireprofile(){
+        document.getElementById("prenomprofile").innerHTML=utilisateur["prenom"];
+        document.getElementById("nomprofile").innerHTML=utilisateur["nom"];
+        document.getElementById("pseudoprofile").innerHTML=utilisateur["pseudo"];
+        document.getElementById("sexprofile").innerHTML=utilisateur["sexe"];
+        document.getElementById("mailprofile").innerHTML=utilisateur["mail"];
+        document.getElementById("photoprofile").src=utilisateur["photo"];
+
+
+
+}
+function lirephoto(){
+    
+    document.getElementById("photophoto").src=utilisateur["photo"];
+    document.getElementById("photophoto2").src=utilisateur["photo"];
+    document.getElementById("photophoto3").src=utilisateur["photo"];
+
+
+
+}
+
+function lireswipe(){
+    const url = "https://proj.ruben-jeaurat.fr/selectgeo"
+    
+    //window.location='./home.html'  
+    fetch(url, {
+        headers: { 'Accept': 'application/json'
+    },
+})
+    .then(response => response.json())
+    .then(response=>{
+    
+        for (var i = 0; i < response.length; i++){
+            if(response[i]["idGeotchatteur"]==5){
+
+            let divbox = document.createElement('div');
+            divbox.className="siwpeBox swipebox"
+            //divbox.style.opacity='0'
+            divbox.id="swipebox"+response[i]["idGeotchatteur"]
+            //divbox.style.transition = 'opacity 4s ease-in-out'
+            
+            divbox.style.display='none';
+
+            let h1nomnomswipe =document.createElement('h1');
+            h1nomnomswipe.innerHTML=response[i]["prenom"];
+
+            let imgswipe = document.createElement('img');
+            imgswipe.style.height='100%';
+
+            imgswipe.src=response[i]["photo"]
+
+            let spanswipe = document.createElement('span');
+            spanswipe.className="bubble dislike"
+            spanswipe.setAttribute('onclick','dislike('+response[i]["idGeotchatteur"]+')');
+            let icondislike = document.createElement('ion-icon');
+            icondislike.name="thumbs-down-outline"
+            spanswipe.appendChild(icondislike)
+
+
+            let spanswipe2 = document.createElement('span');
+            spanswipe2.className="bubble like"
+            spanswipe2.setAttribute('onclick','like('+response[i]["idGeotchatteur"]+')');
+            
+            let iconlike = document.createElement('ion-icon');
+            iconlike.name="thumbs-up-outline"
+            spanswipe2.appendChild(iconlike)
+
+            divbox.appendChild(h1nomnomswipe)
+            divbox.appendChild(imgswipe)
+            divbox.appendChild(spanswipe)
+            divbox.appendChild(spanswipe2)
+            document.getElementById("b1").appendChild(divbox)
+
+            /*document.getElementById("b1").insertAdjacentHTML("beforeend","<div id="+response[i]["idGeotchatteur"]+" class="+"siwpeBox"+">"+
+            "<h1 id="+"nomswipe"+">"+response[i]["prenom"]+"</h1>"+
+            "<img id="+"photoswipe"+" style=" +"height:100%;"+" src="+response[i]["photo"]+" alt="+""+">"+
+                    "<span class=bubble dislike onclick="+"dislike()"+">"+
+                        "<ion-icon name="+"thumbs-down-outline"+"></ion-icon>"+
+                    "</span>"+
+                    "<span class=bubble class=like onclick="+"like()"+">"+
+                        "<ion-icon name="+"thumbs-up-outline"+"></ion-icon>"+
+                    "</span>"+     
+            "</div>")*/
+
+
+
+
+            }
+           
+
+        }
+    })
+    //document.getElementById("nomswipe").innerHTML=utilisateur["nom"];
+    //document.getElementById("photoswipe").innerHTML=utilisateur["photo"];
+    
+
+
+
+}
+
+function lireparametre(){
+    document.getElementById("prenomparametre").innerHTML=utilisateur["prenom"];
+    document.getElementById("nomparametre").innerHTML=utilisateur["nom"];
+    document.getElementById("pseudoparametre").innerHTML=utilisateur["pseudo"];
+    document.getElementById("mailparametre").innerHTML=utilisateur["mail"];
+    document.getElementById("photoparametre").src=utilisateur["photo"];
+
+
+}
+
+
+ function liremsg(talkto){
+    //signIn();
+    
+    
+
+    const url = "https://proj.ruben-jeaurat.fr/selectmess"
+    //var messageBoxt=stevenboxconversation;
+    console.log(sessionStorage.getItem('idtalkto'))
+    //window.location='./home.html'  
+    fetch(url, {
+        headers: { 'Accept': 'application/json'
+        },
+    })
+    .then(response => response.json())
+    .then(response =>  {document.getElementById("messageBox").innerHTML='';
+        for(var i = 0; i < response.length; i++){
+           // console.log(utilisateur)
+            //console.log(response[i]["idEnvoyeur"]==utilisateur["idGeotchatteur"])
+           //console.log(response[i]["idEnvoyeur"]==talkto) 
+           //console.log(response[i]["idRecepteur"])
+           
+           if((response[i]["idEnvoyeur"]==utilisateur["idGeotchatteur"] || response[i]["idEnvoyeur"]==talkto) && (response[i]["idRecepteur"]== utilisateur["idGeotchatteur"]||response[i]["idRecepteur"]==talkto)){
+           
+        let p_msg=document.createElement('p');
+        utilisateur["idGeotchatteur"]==response[i]["idEnvoyeur"]? p_msg.className="from-me" : p_msg.className="from-them"
+        //p_msg.className=r.messages[i].identite;
+
+        let imess=document.createElement('div')
+        imess.className="imessage "+talkto
+        
+        p_msg.innerHTML=response[i]["message"];
+        imess.appendChild(p_msg);
+        document.getElementById("messageBox").appendChild(imess);
+        
+        }  
+        }
+        fetch("https://proj.ruben-jeaurat.fr/selectgeo?id="+talkto, 
+        {
+            headers: { 'Accept': 'application/json'
+        },
+    })
+        .then(response => response.json())
+    .then(response=>{document.getElementById("nommessage").innerHTML=response[0]["prenom"]}
+         )
+       
+        
+    
+    })
+ }
+
+ function envoyermsg(talkto){
+	
+	const url = "https://proj.ruben-jeaurat.fr/modifymess?message="+ document.querySelector('#inputmsg').value+"&idEnvoyeur="+utilisateur["idGeotchatteur"]+"&idRecepteur="+talkto
+     
+	//let formData = new FormData();
+    /*formData.append("message",document.querySelector('#message').value)
+    //formData.append('idMessage', '7');
+    formData.append('dateMessage',Date.getDate());
+    
+    formData.append("idEnvoyeur",utilisateur["idGeotchatteur"]);
+    formData.append("idRecepteur",talkto);
+    formData.append("active",0);
+    formData.append("creation","0000-00-00 00:00:00");
+    formData.append("modification","0000-00-00 00:00:00");*/
+    fetch(url, {
+    method: 'get',
+    headers: { 'Accept': 'application/json',"Content-Type": "multipart/form-data" 
+
+    },
+    //body: formData
+})
+
+    .then(document.querySelector('#inputmsg').value="")
+    .catch(response=>console.log(response))
+
+
+	
+}
+
+
+function calcCrow(lat1, lon1, lat2, lon2) 
+{
+  var R = 6371; // km
+  var dLat = toRad(lat2-lat1);
+  var dLon = toRad(lon2-lon1);
+  var lat1 = toRad(lat1);
+  var lat2 = toRad(lat2);
+
+  var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  var d = R * c;
+  return d;
+}
+
+// Converts numeric degrees to radians
+function toRad(Value) 
+{
+    return Value * Math.PI / 180;
+}
+
--- www/js/signIn.js
+++ www/js/signIn.js
@@ -2,12 +2,57 @@
 document.addEventListener('deviceready', onDeviceReady, true);
 
 
+
+
+var longitude;
+var latitude;
+
+takeLocation ();
+
 function onDeviceReady() {
     
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version)
     document.getElementById('buttonLoard').addEventListener('click', takePicturFromGalerie, false);
     document.getElementById('picture').addEventListener('click', takePic, false);
+    
+    
 }
+
+function takeLocation () {
+    
+    const options = {
+        maximumAge: 3000,
+        timeout: 5000, 
+        enableHighAccuracy: true 
+    }
+
+    var onSuccess = function(position) {
+        
+        
+        console.log('Latitude: '          + position.coords.latitude          + '\n' +
+            'Longitude: '         + position.coords.longitude         + '\n' +
+            'Altitude: '          + position.coords.altitude          + '\n' +
+            'Accuracy: '          + position.coords.accuracy          + '\n' +
+            'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+            'Heading: '           + position.coords.heading           + '\n' +
+            'Speed: '             + position.coords.speed             + '\n' +
+            'Timestamp: '         + position.timestamp                + '\n');
+            latitude=position.coords.latitude ;
+            longitude=position.coords.longitude;
+
+    };
+
+    // onError Callback receives a PositionError object
+    //
+    function onError(error) {
+        alert('code: '    + error.code    + '\n' +
+            'message: ' + error.message + '\n');
+    }
+
+    navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
+}
+
+
 
 
 function takePicturFromGalerie() {
@@ -17,6 +62,7 @@ function takePicturFromGalerie() {
         sourceType: Camera.PictureSourceType.SAVEDPHOTOALBUM
     }
     navigator.camera.getPicture(onSuccess, onFail, options)
+    
 }
 
 function takePic() {
@@ -35,7 +81,7 @@ function onFail(message) {
     alert("Echec car :" + message)
 }
 
-function signIn() {
+/*function signIn() {
     const url = "http://erickstattner.com/service/user.php"
     var login = document.getElementById("login").value;
     var password = document.getElementById("password").value;
@@ -67,4 +113,81 @@ function signIn() {
 
      /* on wait */
    
+ 
+
+ //steven
+ function signIn() {
+    
+    var login = document.getElementById("login").value;
+    var password = document.getElementById("password").value;
+    //window.location='./home.html';
+    //console.log('dfdf')
+    // var bodyFormData = new FormData();
+    // bodyFormData.append('id', '15');
+
+    fetch("https://proj.ruben-jeaurat.fr/api/selectlog?pseudo="+login+"&mdp="+password, {
+        headers: { 'Accept': 'application/json'
+        },
+    })
+    .then(response =>response.json())
+    .then(response=>
+        fetch("https://proj.ruben-jeaurat.fr/api/modifygeo?id="+response[0]["idGeotchatteur"]+"?DerniereLongitude="+longitude+"?DerniereLatitude="+latitude, {
+        headers: { 'Accept': 'application/json'},
+    })
+    .then(response=>{
+            fetch("https://proj.ruben-jeaurat.fr/api/selectlog?pseudo="+login+"&mdp="+password, {
+                headers: { 'Accept': 'application/json'
+                },
+            })
+           .then(response =>response.json())
+           .then(response =>{sessionStorage.setItem('utilisateur2',JSON.stringify(response[0]))})
+           .then(console.log(sessionStorage.getItem('utilisateur2')))
+           .then(response =>{window.location.assign('./home.html')})
+           .catch(response=>{console.log(response );window.location='./signIn.html';return false})
+        
+            }
+        )
+       )
+    
+
+
+   
+     
+    
+    
+    
+    //.then(window.location='./home.html')
+    
  }
+
+ /*function signIn() {
+    
+    console.log(this);
+
+    if(true){
+    var login = document.getElementById("login").value;
+    var password = document.getElementById("password").value;
+    }
+    else{
+        
+    } 
+    var url = "https://proj.ruben-jeaurat.fr/selectgeo?id=5"
+    
+    axios({
+        method: "get",
+        url: url,
+        headers: {'Accept': 'application/json'  },
+    })
+    .then(response =>response.data)
+   .then(response =>{utilisateur=response[0];})
+   .then(response =>console.log(utilisateur))
+    
+   
+    
+   return false
+     
+    
+    
+
+    
+ }*/
