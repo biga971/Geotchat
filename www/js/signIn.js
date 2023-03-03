01cb4bf58fdeb462c4edd88f1d0116a59daa161e
sabassi <abassisteven@gmail.com>
2023-03-02 20:28:38 -0400

    photto

--- www/js/signIn.js
+++ www/js/signIn.js
@@ -117,7 +117,7 @@ function onFail(message) {
 
  //steven
  function signIn() {
-    
+    takeLocation ();
     var login = document.getElementById("login").value;
     var password = document.getElementById("password").value;
     //window.location='./home.html';
